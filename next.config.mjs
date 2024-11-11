4fe7871d1b357947138caced90442c0f98606a8e
simondatking school <69970561+simondatking@users.noreply.github.com>
2024-11-11 11:05:34 +0100

    gh-pages are pain

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/TE4_24-25_PokemonWeb",
+    basePath: "/TE4_24-25_PokemonWeb/",
   
     /**
      * Disable server-based image optimization. Next.js does not support
